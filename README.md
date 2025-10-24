210876870f94fd7a91a2803a27e7c26b17f25c70
Kim Dongwhee (dk33327) <dk33327@austin.utexas.edu@ecen-d24468.austin.utexas.edu>
2025-10-24 12:03:11 -0500

    2025-10-24 QEC

---
+++ README.md
@@ -0,0 +1,7 @@
+## Environmental Setup (Conda Recommended)
+```bash
+$ conda create --name [conda name]
+$ (Every time) conda activate [conda name]
+$ conda install pip
+$ python environmental_setup.py
+```
---
+++ environmental_setup.py
@@ -0,0 +1,8 @@
+import os
+
+os.system(pip install qiskit==2.1.2)
+os.system(pip install matplotlib==3.10.0)
+os.system(pip install pylatexenc)
+os.system(pip install qiskit-aer==0.17.1)
+os.system(pip install numpy==2.0.2)
+os.system(pip install qiskit_ibm_runtime==0.41.1)
