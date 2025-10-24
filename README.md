ed8a0c11a16ee31385f10d6215aa9fb75aaae9e0
Kim Dongwhee (dk33327) <dk33327@austin.utexas.edu@ecen-d24468.austin.utexas.edu>
2025-10-24 17:29:36 -0500

    2025-10-24 License Update

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# Quantum Error Correction (QEC) Exercise (Will continue to update)
+
+# This project is licensed under the terms of the MIT
+
+## Author
+
+**Dongwhee Kim**
+- [```Google Scholar```](https://scholar.google.com/citations?user=8xzqA8YAAAAJ&hl=ko&oi=ao)
+- [```ORCiD```](https://orcid.org/0009-0007-1673-1931?fbclid=PAAabkpwNHesKweJ6F2eGZDnFa2sch2211hf6ZY825YKuli5V7lcN7VIfT0CA)
+- [```LinkedIn```](https://www.linkedin.com/in/dongwhee-kim-5753a8290)
+
 ## Environmental Setup (Conda Recommended)
 ```bash
 $ conda create --name [conda name]
--- environmental_setup.py
+++ environmental_setup.py
@@ -1,8 +1,32 @@
 import os
+import sys
 
-os.system(pip install qiskit==2.1.2)
-os.system(pip install matplotlib==3.10.0)
-os.system(pip install pylatexenc)
-os.system(pip install qiskit-aer==0.17.1)
-os.system(pip install numpy==2.0.2)
-os.system(pip install qiskit_ibm_runtime==0.41.1)
+def install_package(package):
+    print(f"Installing {package}...")
+    command = f'"{sys.executable}" -m pip install {package}'
+    print(f"Executing: {command}")
+    result = os.system(command)
+    
+    if result != 0:
+        print(f"ERROR: Failed to install {package}. Exiting.")
+        sys.exit(1)
+    else:
+        print(f"Successfully installed {package}\n")
+
+# Package list
+packages = [
+    "qiskit==2.1.2",
+    "matplotlib==3.10.0",
+    "pylatexenc",
+    "qiskit-aer==0.17.1",
+    "numpy==2.0.2",
+    "qiskit_ibm_runtime==0.41.1"
+]
+
+print("Starting environmental setup...")
+
+# Install package
+for pkg in packages:
+    install_package(pkg)
+
+print("All specified packages have been processed successfully.")
