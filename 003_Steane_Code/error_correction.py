6f3ae10727bc73162facfeec88b7cae775b73995
Kim Dongwhee (dk33327) <dk33327@austin.utexas.edu@ecen-d24468.austin.utexas.edu>
2025-10-29 18:03:47 -0500

    2025-10-29 Steane Code Draft

--- 003_Steane_Code/error_correction.py
+++ 003_Steane_Code/error_correction.py
@@ -4,132 +4,117 @@ import sys
 
 # Applies correction operations (X, Z) based on the measured syndrome bits
 def error_correction_func(qc):
-    """
-    Applies correction operations (X, Z) based on the measured syndrome bits
-    c[0]...c[5], using classically controlled gates (if_test).
-
-    This logic is the inverse of the syndrome map defined in error_detection.py.
-    
-    X-Error Correction (based on Z-stabilizers):
-    Syndrome bits: c[2]c[1]c[0]
-    c[0] = Z0 Z2 Z4 Z6
-    c[1] = Z1 Z2 Z5 Z6
-    c[2] = Z3 Z4 Z5 Z6
-    
-    Syndrome (c[2]c[1]c[0]) -> Apply X gate to:
-    - 001 -> q[0]
-    - 010 -> q[1]
-    - 011 -> q[2]
-    - 100 -> q[3]
-    - 101 -> q[4]
-    - 110 -> q[5]
-    - 111 -> q[6]
-
-    Z-Error Correction (based on X-stabilizers):
-    Syndrome bits: c[5]c[4]c[3]
-    c[3] = X0 X2 X4 X6
-    c[4] = X1 X2 X5 X6
-    c[5] = X3 X4 X5 X6
-    
-    Syndrome (c[5]c[4]c[3]) -> Apply Z gate to:
-    - 001 -> q[0]
-    - 010 -> q[1]
-    - 011 -> q[2]
-    - 100 -> q[3]
-    - 101 -> q[4]
-    - 110 -> q[5]
-    - 111 -> q[6]
-    """
-    
     # Get the 7-bit classical register
     c_reg = qc.cregs[0]
 
-    # --- X-Error Correction (using syndrome c[2]c[1]c[0]) ---
+    # --- X-Error Correction ---
 
-    # Case 001 -> X(0)
-    with qc.if_test((c_reg[2], 0)) as else_0:
+    # syndrome table
+    # c0 c1 c2
+    # 000 -> no bit-flip error
+    # 100 -> q0 flip
+    # 010 -> q1 flip
+    # 110 -> q2 flip
+    # 001 -> q3 flip
+    # 101 -> q4 flip
+    # 011 -> q5 flip
+    # 111 -> q6 flip
+
+    # 100
+    with qc.if_test((c_reg[0], 1)) as else_0:
         with qc.if_test((c_reg[1], 0)) as else_1:
-            with qc.if_test((c_reg[0], 1)):
+            with qc.if_test((c_reg[2], 0)):
                 qc.x(0)
     
-    # Case 010 -> X(1)
-    with qc.if_test((c_reg[2], 0)) as else_2:
+    # 010
+    with qc.if_test((c_reg[0], 0)) as else_2:
         with qc.if_test((c_reg[1], 1)) as else_3:
-            with qc.if_test((c_reg[0], 0)):
+            with qc.if_test((c_reg[2], 0)):
                 qc.x(1)
 
-    # Case 011 -> X(2)
-    with qc.if_test((c_reg[2], 0)) as else_4:
+    # 110
+    with qc.if_test((c_reg[0], 1)) as else_4:
         with qc.if_test((c_reg[1], 1)) as else_5:
-            with qc.if_test((c_reg[0], 1)):
+            with qc.if_test((c_reg[2], 0)):
                 qc.x(2)
 
-    # Case 100 -> X(3)
-    with qc.if_test((c_reg[2], 1)) as else_6:
+    # 001
+    with qc.if_test((c_reg[0], 0)) as else_6:
         with qc.if_test((c_reg[1], 0)) as else_7:
-            with qc.if_test((c_reg[0], 0)):
+            with qc.if_test((c_reg[2], 1)):
                 qc.x(3)
 
-    # Case 101 -> X(4)
-    with qc.if_test((c_reg[2], 1)) as else_8:
+    # 101
+    with qc.if_test((c_reg[0], 1)) as else_8:
         with qc.if_test((c_reg[1], 0)) as else_9:
-            with qc.if_test((c_reg[0], 1)):
+            with qc.if_test((c_reg[2], 1)):
                 qc.x(4)
 
-    # Case 110 -> X(5)
-    with qc.if_test((c_reg[2], 1)) as else_10:
+    # 011
+    with qc.if_test((c_reg[0], 0)) as else_10:
         with qc.if_test((c_reg[1], 1)) as else_11:
-            with qc.if_test((c_reg[0], 0)):
+            with qc.if_test((c_reg[2], 1)):
                 qc.x(5)
 
-    # Case 111 -> X(6)
-    with qc.if_test((c_reg[2], 1)) as else_12:
+    # 111
+    with qc.if_test((c_reg[0], 1)) as else_12:
         with qc.if_test((c_reg[1], 1)) as else_13:
-            with qc.if_test((c_reg[0], 1)):
+            with qc.if_test((c_reg[2], 1)):
                 qc.x(6)
 
-    # --- Z-Error Correction (using syndrome c[5]c[4]c[3]) ---
+    # --- Z-Error Correction ---
     
-    # Case 001 -> Z(0)
-    with qc.if_test((c_reg[5], 0)) as else_14:
+    # syndrome table
+    # c3 c4 c5
+    # 000 -> no phase-flip error
+    # 100 -> q0 flip
+    # 010 -> q1 flip
+    # 110 -> q2 flip
+    # 001 -> q3 flip
+    # 101 -> q4 flip
+    # 011 -> q5 flip
+    # 111 -> q6 flip
+
+    # 100
+    with qc.if_test((c_reg[3], 1)) as else_14:
         with qc.if_test((c_reg[4], 0)) as else_15:
-            with qc.if_test((c_reg[3], 1)):
+            with qc.if_test((c_reg[4], 0)):
                 qc.z(0)
     
-    # Case 010 -> Z(1)
-    with qc.if_test((c_reg[5], 0)) as else_16:
+    # 010
+    with qc.if_test((c_reg[3], 0)) as else_16:
         with qc.if_test((c_reg[4], 1)) as else_17:
-            with qc.if_test((c_reg[3], 0)):
+            with qc.if_test((c_reg[5], 0)):
                 qc.z(1)
 
-    # Case 011 -> Z(2)
-    with qc.if_test((c_reg[5], 0)) as else_18:
+    # 110
+    with qc.if_test((c_reg[3], 1)) as else_18:
         with qc.if_test((c_reg[4], 1)) as else_19:
-            with qc.if_test((c_reg[3], 1)):
+            with qc.if_test((c_reg[5], 0)):
                 qc.z(2)
 
-    # Case 100 -> Z(3)
-    with qc.if_test((c_reg[5], 1)) as else_20:
+    # 001
+    with qc.if_test((c_reg[3], 0)) as else_20:
         with qc.if_test((c_reg[4], 0)) as else_21:
-            with qc.if_test((c_reg[3], 0)):
+            with qc.if_test((c_reg[5], 1)):
                 qc.z(3)
 
-    # Case 101 -> Z(4)
-    with qc.if_test((c_reg[5], 1)) as else_22:
+    # 101
+    with qc.if_test((c_reg[3], 1)) as else_22:
         with qc.if_test((c_reg[4], 0)) as else_23:
-            with qc.if_test((c_reg[3], 1)):
+            with qc.if_test((c_reg[5], 1)):
                 qc.z(4)
 
-    # Case 110 -> Z(5)
-    with qc.if_test((c_reg[5], 1)) as else_24:
+    # 011
+    with qc.if_test((c_reg[3], 0)) as else_24:
         with qc.if_test((c_reg[4], 1)) as else_25:
-            with qc.if_test((c_reg[3], 0)):
+            with qc.if_test((c_reg[5], 1)):
                 qc.z(5)
 
-    # Case 111 -> Z(6)
-    with qc.if_test((c_reg[5], 1)) as else_26:
+    # 111
+    with qc.if_test((c_reg[3], 1)) as else_26:
         with qc.if_test((c_reg[4], 1)) as else_27:
-            with qc.if_test((c_reg[3], 1)):
+            with qc.if_test((c_reg[5], 1)):
                 qc.z(6)
     
     return
